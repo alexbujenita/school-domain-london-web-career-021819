d8b9c041603eaca7e2774d255936515802670139
Alex Bujenita <dekc89@gmail.com>
2019-02-20 20:43:20 +0000

    school done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if roster.key?(grade)
+      roster[grade].push(student_name)
+    else
+      roster[grade] = []
+      roster[grade].push(student_name)
+    end
+  end
+
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted_hash = {}
+    roster.each do |k_int, v_arr|
+      sorted = v_arr.sort
+      sorted_hash[k_int] = sorted
+    end
+    sorted_hash
+  end
+end
